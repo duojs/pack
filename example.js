4bcad5eb47b34158b7dfd9ae0efa202b11aeb454
Matthew Mueller <mattmuelle@gmail.com>
2014-03-30 23:25:41 -0300

    added mkdirp to pack

--- example.js
+++ example.js
@@ -6,12 +6,12 @@ var co = require('co');
 var Pack = require('./');
 
 co(function *() {
-  var pack = Pack('build.js');
+  var pack = Pack('build/build.js');
   yield pack({ id: 'a', src: 'module.exports = "hi there"', deps: { 'b': 'b' }});
   yield pack({ id: 'b', src: 'module.exports = "whatever"', deps: {}}, true);
 })(done);
 
 function done(err) {
-  if (err) console.log(err);;
+  if (err) console.log(err);
   console.log('all packed!');
 }
