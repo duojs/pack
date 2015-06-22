duojs/pack

commit 8d6c6264cf87db9f39788232ba3bea69209ec20a

not caching module until successful resolve (fixes duojs/duo#421)

test/index.js
@@ -223,6 +223,15 @@ describe('Pack', function(){
     var css = pack.pack('some/dir/a');
     assert.equal('section { background: url("images/b.png"); }', css);
   })
+
+  it('should not cache broken packages', function() {
+    var map = require('./fixtures/error-cache');
+    var pack = Pack(map);
+    var js = pack.pack('a');
+
+    var ret = evaluate(js).require(1);
+    assert.strictEqual(ret.b1, ret.b2);
+  })
 })
 
 function evaluate(js, ctx){

lib/js/require.js
@@ -47,15 +47,18 @@ function outer(modules, cache, entries){
    */
 
   function call(id, require){
-    var m = cache[id] = { exports: {} };
+    var m = { exports: {} };
     var mod = modules[id];
     var name = mod[2];
     var fn = mod[0];
 
     fn.call(m.exports, function(req){
       var dep = modules[id][1][req];
-      return require(dep ? dep : req);
+      return require(dep || req);
     }, m, m.exports, outer, modules, cache, entries);
+
+    // store to cache after successful resolve
+    cache[id] = m;
 
     // expose as `name`.
     if (name) cache[name] = cache[id];

test/fixtures/error-cache.js
@@ -1,0 +1,13 @@
+exports.a = {
+  id: 'a',
+  type: 'js',
+  src: 'var b1, b2; try { b1 = require("./b"); } catch (err) { b1 = err; }\ntry { b2 = require("./b"); } catch (err) { b2 = err; }\nexports.b1 = b1; exports.b2 = b2;',
+  entry: true,
+  deps: { './b': 'b' }
+};
+
+exports.b = {
+  id: 'b',
+  type: 'js',
+  src: 'throw "fail";'
+};

commit 76ff11a8caf16a1381f519e69a54f8375e8a2d49

test: Expose a circular dependency resolution bug
It looks like 8d6c6264cf87db9f39788232ba3bea69209ec20a broke our
ability to `require()` circular/deeply-nested dependencies.  This patch
adds a (failing) test exposing the bug.

test/fixtures/circular.js
@@ -1,0 +1,209 @@
+exports.A = {
+  id: "A",
+  type: "js",
+  src: "console.log(require('./B'));",
+  entry: true,
+  deps: {
+    "./B": "B"
+  }
+};
+exports.B = {
+  id: "B",
+  type: "js",
+  src: "exports.C = require('./C').C",
+  deps: {
+    "./C": "C"
+  }
+};
+exports.C = {
+  id: "C",
+  type: "js",
+  src: "exports.D = require('./D').D",
+  deps: {
+    "./D": "D"
+  }
+};
+exports.D = {
+  id: "D",
+  type: "js",
+  src: "exports.E = require('./E').E",
+  deps: {
+    "./E": "E"
+  }
+};
+exports.E = {
+  id: "E",
+  type: "js",
+  src: "exports.F = require('./F').F",
+  deps: {
+    "./F": "F"
+  }
+};
+exports.F = {
+  id: "F",
+  type: "js",
+  src: "exports.G = require('./G').G",
+  deps: {
+    "./G": "G"
+  }
+};
+exports.G = {
+  id: "G",
+  type: "js",
+  src: "exports.H = require('./H').H",
+  deps: {
+    "./H": "H"
+  }
+};
+exports.H = {
+  id: "H",
+  type: "js",
+  src: "exports.I = require('./I').I",
+  deps: {
+    "./I": "I"
+  }
+};
+exports.I = {
+  id: "I",
+  type: "js",
+  src: "exports.J = require('./J').J",
+  deps: {
+    "./J": "J"
+  }
+};
+exports.J = {
+  id: "J",
+  type: "js",
+  src: "exports.K = require('./K').K",
+  deps: {
+    "./K": "K"
+  }
+};
+exports.K = {
+  id: "K",
+  type: "js",
+  src: "exports.L = require('./L').L",
+  deps: {
+    "./L": "L"
+  }
+};
+exports.L = {
+  id: "L",
+  type: "js",
+  src: "exports.M = require('./M').M",
+  deps: {
+    "./M": "M"
+  }
+};
+exports.M = {
+  id: "M",
+  type: "js",
+  src: "exports.N = require('./N').N",
+  deps: {
+    "./N": "N"
+  }
+};
+exports.N = {
+  id: "N",
+  type: "js",
+  src: "exports.O = require('./O').O",
+  deps: {
+    "./O": "O"
+  }
+};
+exports.O = {
+  id: "O",
+  type: "js",
+  src: "exports.P = require('./P').P",
+  deps: {
+    "./P": "P"
+  }
+};
+exports.P = {
+  id: "P",
+  type: "js",
+  src: "exports.Q = require('./Q').Q",
+  deps: {
+    "./Q": "Q"
+  }
+};
+exports.Q = {
+  id: "Q",
+  type: "js",
+  src: "exports.R = require('./R').R",
+  deps: {
+    "./R": "R"
+  }
+};
+exports.R = {
+  id: "R",
+  type: "js",
+  src: "exports.S = require('./S').S",
+  deps: {
+    "./S": "S"
+  }
+};
+exports.S = {
+  id: "S",
+  type: "js",
+  src: "exports.T = require('./T').T",
+  deps: {
+    "./T": "T"
+  }
+};
+exports.T = {
+  id: "T",
+  type: "js",
+  src: "exports.U = require('./U').U",
+  deps: {
+    "./U": "U"
+  }
+};
+exports.U = {
+  id: "U",
+  type: "js",
+  src: "exports.V = require('./V').V",
+  deps: {
+    "./V": "V"
+  }
+};
+exports.V = {
+  id: "V",
+  type: "js",
+  src: "exports.W = require('./W').W",
+  deps: {
+    "./W": "W"
+  }
+};
+exports.W = {
+  id: "W",
+  type: "js",
+  src: "exports.X = require('./X').X",
+  deps: {
+    "./X": "X"
+  }
+};
+exports.X = {
+  id: "X",
+  type: "js",
+  src: "exports.Y = require('./Y').Y",
+  deps: {
+    "./Y": "Y"
+  }
+};
+exports.Y = {
+  id: "Y",
+  type: "js",
+  src: "exports.Z = require('./Z').Z",
+  deps: {
+    "./Z": "Z"
+  }
+};
+exports.Z = {
+  id: "Z",
+  type: "js",
+  src: "module.exports = require('./A');",
+  deps: {
+    "./A": "A"
+  }
+};

test/index.js
@@ -252,6 +252,13 @@ describe('Pack', function(){
     var ret = evaluate(js.code).require(1);
     assert.strictEqual(ret.b1, ret.b2);
   })
+
+  it('should support circular dependencies', function(){
+    var map = require('./fixtures/circular');
+    var pack = new Pack(map);
+    var js = pack.pack('A');
+    evaluate(js.code);
+  })
 })
 
 function evaluate(js, ctx){