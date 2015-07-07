duojs/pack

commit 8d6c6264cf87db9f39788232ba3bea69209ec20a

not caching module until successful resolve (fixes duojs/duo#421)

lib/js/require.js
@@ -47,15 +47,18 @@ function outer(modules, cache, entries){
    */
 
   function call(id, require){
-    var m = cache[id] = { exports: {} };
+    var m = { exports: {} };
     var mod = modules[id];
     var name = mod[2];
     var fn = mod[0];
 
     fn.call(m.exports, function(req){
       var dep = modules[id][1][req];
-      return require(dep ? dep : req);
+      return require(dep || req);
     }, m, m.exports, outer, modules, cache, entries);
+
+    // store to cache after successful resolve
+    cache[id] = m;
 
     // expose as `name`.
     if (name) cache[name] = cache[id];

test/fixtures/error-cache.js
@@ -1,0 +1,13 @@
+exports.a = {
+  id: 'a',
+  type: 'js',
+  src: 'var b1, b2; try { b1 = require("./b"); } catch (err) { b1 = err; }\ntry { b2 = require("./b"); } catch (err) { b2 = err; }\nexports.b1 = b1; exports.b2 = b2;',
+  entry: true,
+  deps: { './b': 'b' }
+};
+
+exports.b = {
+  id: 'b',
+  type: 'js',
+  src: 'throw "fail";'
+};

test/index.js
@@ -223,6 +223,15 @@ describe('Pack', function(){
     var css = pack.pack('some/dir/a');
     assert.equal('section { background: url("images/b.png"); }', css);
   })
+
+  it('should not cache broken packages', function() {
+    var map = require('./fixtures/error-cache');
+    var pack = Pack(map);
+    var js = pack.pack('a');
+
+    var ret = evaluate(js).require(1);
+    assert.strictEqual(ret.b1, ret.b2);
+  })
 })
 
 function evaluate(js, ctx){

commit 511da67a8e6f08c8ca2e5b68ec3f01c323b561cc

require: Fix circular dependency loading
This patch fixes a bug introduced in
8d6c6264cf87db9f39788232ba3bea69209ec20a, where circular dependency
loading would cause the call stack to fill and eventually to a
`RangeError` being thrown.  This patch does not cause broken (throwing)
modules from being cached.

Related: duojs/duo#474.

lib/js/require.js
@@ -47,21 +47,26 @@ function outer(modules, cache, entries){
    */
 
   function call(id, require){
-    var m = { exports: {} };
+    var m = cache[id] = { exports: {} };
     var mod = modules[id];
     var name = mod[2];
     var fn = mod[0];
+    var threw = true;
 
-    fn.call(m.exports, function(req){
-      var dep = modules[id][1][req];
-      return require(dep || req);
-    }, m, m.exports, outer, modules, cache, entries);
-
-    // store to cache after successful resolve
-    cache[id] = m;
-
-    // expose as `name`.
-    if (name) cache[name] = cache[id];
+    try {
+      fn.call(m.exports, function(req){
+        var dep = modules[id][1][req];
+        return require(dep ? dep : req);
+      }, m, m.exports, outer, modules, cache, entries);
+      threw = false;
+    } finally {
+      if (threw) {
+        delete cache[id];
+      } else if (name) {
+        // expose as 'name'.
+        cache[name] = cache[id];
+      }
+    }
 
     return cache[id].exports;
   }